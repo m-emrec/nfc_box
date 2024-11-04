5e2eabcdd20cd66280661d1f2025b629e9bb128b
memrec45 <65484287+memrec45@users.noreply.github.com>
2024-11-04 14:47:36 +0300

    Add:NfcRead page added

--- lib/features/nfc/view/read_nfc_page.dart
+++ lib/features/nfc/view/read_nfc_page.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
+import 'package:nfc_box/core/constants/app_paddings.dart';
+import 'package:nfc_box/core/extensions/context_extension.dart';
+import 'package:nfc_box/core/utils/widgets/buttons/buttons_import.dart';
+import 'package:nfc_box/core/utils/widgets/buttons/inline_text_button.dart';
+
+import '../../../core/constants/app_assets.dart';
+import '../../../core/constants/colors.dart';
 
 class ReadNfcPage extends ConsumerStatefulWidget {
   const ReadNfcPage({super.key});
@@ -11,6 +21,38 @@ class ReadNfcPage extends ConsumerStatefulWidget {
 class _ReadNfcPageState extends ConsumerState<ReadNfcPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var reading = 'Reading...';
+    var pleaseHoldYourPhoneNearTheNFCTag =
+        'Please hold your phone near the NFC tag';
+    var cancel = 'Cancel';
+    return Scaffold(
+      appBar: AppBar(),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Lottie.asset(AppAssets.readingNfcAni),
+            Text(
+              reading,
+              style: context.textTheme.titleLarge,
+            ),
+            Text(
+              pleaseHoldYourPhoneNearTheNFCTag,
+              style: context.textTheme.bodyMedium,
+            ),
+            MaxGap(AppPaddings.lPadding),
+            TextButton(
+              onPressed: () => context.go("/"),
+              child: Text(
+                cancel,
+                style: TextStyle(color: AppColors.accentError[70]),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
